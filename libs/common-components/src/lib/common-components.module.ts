42ba5ac95385dc59f047dfc8aef246dee92caaf5
DiGeomel <3295889+digeomel@users.noreply.github.com>
2023-05-09 11:02:08 +0200

    build: correctly import FontAwesome icons (tested only with the viewer component, more may be needed for other libs)

--- libs/common-components/src/lib/common-components.module.ts
+++ libs/common-components/src/lib/common-components.module.ts
@@ -1,83 +1,104 @@
-import {NgModule} from '@angular/core';
-import {CommonModule} from '@angular/common';
-import {TopToolbarComponent} from './top-toolbar/top-toolbar.component';
-import {SidePanelComponent} from './side-panel/side-panel.component';
-import {ButtonComponent} from './button/button.component';
-import {LogoComponent} from './logo/logo.component';
-import {TooltipComponent} from './tooltip/tooltip.component';
-import {FontAwesomeModule} from '@fortawesome/angular-fontawesome';
-import {library} from '@fortawesome/fontawesome-svg-core';
-import {fas} from '@fortawesome/free-solid-svg-icons';
-import {far} from '@fortawesome/free-regular-svg-icons';
-import {Api, ConfigService,} from "./config.service";
-import {ModalService} from "./modal.service";
-import {ModalComponent} from './modal/modal.component';
-import {BrowseFilesModalComponent} from './browse-files-modal/browse-files-modal.component';
-import {FileModel, FileService, FileUtil, Utils} from "./file.service";
-import {DocumentComponent} from './document/document.component';
-import {PageComponent} from './page/page.component';
-import {HighlightSearchPipe, SanitizeHtmlPipe, SanitizeResourceHtmlPipe, SanitizeStylePipe} from "./pipes";
-import {UploadFileZoneComponent} from './upload-file-zone/upload-file-zone.component';
-import {UploadFilesService} from "./upload-files.service";
-import {DndDirective} from './dnd.directive';
-import {ScrollableDirective} from './scrollable.directive';
-import {MouseWheelDirective} from './mousewheel.directive';
-import {NavigateService} from "./navigate.service";
-import {PagePreloadService} from "./page-preload.service";
-import {ZoomDirective} from './zoom.directive';
-import {ZoomService} from "./zoom.service";
-import {SelectComponent} from './select/select.component';
-import {DisabledCursorDirective} from './disabled-cursor.directive';
-import {RotationDirective} from './rotation.directive';
-import {InitStateComponent} from './init-state/init-state.component';
-import {RenderPrintService} from "./render-print.service";
-import {RenderPrintDirective} from './render-print.directive';
-import {ErrorModalComponent} from './error-modal/error-modal.component';
-import {PasswordRequiredComponent} from './password-required/password-required.component';
-import {ExceptionMessageService} from "./exception-message.service";
-import {PasswordService} from "./password.service";
-import {ErrorInterceptorService} from "./error-interceptor.service";
-import {SearchComponent} from './search/search.component';
-import {SearchableDirective} from './searchable.directive';
-import {SearchService} from "./search.service";
-import {WindowService} from "./window.service";
-import {ViewportService} from "./viewport.service";
-import {TabbedToolbarsComponent} from './tabbed-toolbars/tabbed-toolbars.component';
-import {TabComponent} from "./tab/tab.component";
-import {TabsComponent} from "./tabs/tabs.component";
-import {FormattingService} from "./formatting.service";
-import {ColorPickerComponent} from './color-picker/color-picker.component';
-import {FormattingDirective} from './formatting.directive';
-import {BackFormattingService} from "./back-formatting.service";
-import {OnCloseService} from "./on-close.service";
-import {SuccessModalComponent} from './success-modal/success-modal.component';
-import {EditorDirective} from './editor.directive';
-import {LoadingMaskComponent} from './loading-mask/loading-mask.component';
-import {LoadingMaskService} from './loading-mask.service';
-import {LoadingMaskInterceptorService} from "./loading-mask-interceptor.service";
-import {TabActivatorService} from "./tab-activator.service";
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import {
+  FaIconLibrary,
+  FontAwesomeModule,
+} from '@fortawesome/angular-fontawesome';
+import {
+  faCheck,
+  faChevronLeft,
+  faChevronRight,
+  faCircleNotch,
+  faCloudDownloadAlt,
+  faExclamationTriangle,
+  faEye,
+  faFile,
+  faFolderOpen,
+  faInfoCircle,
+  faKey,
+  faLevelUpAlt,
+  faTimes,
+} from '@fortawesome/free-solid-svg-icons';
+import { TranslateModule } from '@ngx-translate/core';
+import { NgClickOutsideDirective } from 'ng-click-outside2';
+import { AddDynamicComponentService } from './add-dynamic-component.service';
+import { BackFormattingService } from './back-formatting.service';
+import { BrowseFilesModalComponent } from './browse-files-modal/browse-files-modal.component';
+import { ButtonComponent } from './button/button.component';
+import { ColorPickerComponent } from './color-picker/color-picker.component';
+import { Api, ConfigService } from './config.service';
+import { ContextMenuComponent } from './context-menu/context-menu.component';
+import { DisabledCursorDirective } from './disabled-cursor.directive';
+import { DndDirective } from './dnd.directive';
+import { DocumentComponent } from './document/document.component';
 import {
   DropDownComponent,
   DropDownItemComponent,
   DropDownItemsComponent,
-  DropDownToggleComponent
+  DropDownToggleComponent,
 } from './drop-down/drop-down.component';
-import {NgClickOutsideDirective} from 'ng-click-outside2';
-import {LeftSideBarComponent} from './left-side-bar/left-side-bar.component';
-import {TooltipDirective} from './tooltip.directive';
-import {AddDynamicComponentService} from "./add-dynamic-component.service";
-import {HostDynamicDirective} from './host-dynamic.directive';
-import {HostingDynamicComponentService} from "./hosting-dynamic-component.service";
-import {ResizingComponent} from './resizing/resizing.component';
-import {TopTabComponent} from './top-tab/top-tab.component';
-import {TopTabActivatorService} from "./top-tab-activator.service";
-import {TextMenuComponent} from './text-menu/text-menu.component';
-import {ContextMenuComponent} from './context-menu/context-menu.component';
-import {PageMarkerDirective} from './page-marker.directive';
-import {TranslateModule} from '@ngx-translate/core';
-import {ThumbnailsComponent} from './thumbnails/thumbnails.component';
+import { EditorDirective } from './editor.directive';
+import { ErrorInterceptorService } from './error-interceptor.service';
+import { ErrorModalComponent } from './error-modal/error-modal.component';
+import { ExceptionMessageService } from './exception-message.service';
+import { FileModel, FileService, FileUtil, Utils } from './file.service';
+import { FormattingDirective } from './formatting.directive';
+import { FormattingService } from './formatting.service';
+import { HostDynamicDirective } from './host-dynamic.directive';
+import { HostingDynamicComponentService } from './hosting-dynamic-component.service';
+import { InitStateComponent } from './init-state/init-state.component';
+import { LeftSideBarComponent } from './left-side-bar/left-side-bar.component';
+import { LoadingMaskInterceptorService } from './loading-mask-interceptor.service';
+import { LoadingMaskService } from './loading-mask.service';
+import { LoadingMaskComponent } from './loading-mask/loading-mask.component';
+import { LogoComponent } from './logo/logo.component';
+import { ModalService } from './modal.service';
+import { ModalComponent } from './modal/modal.component';
+import { MouseWheelDirective } from './mousewheel.directive';
+import { NavigateService } from './navigate.service';
+import { OnCloseService } from './on-close.service';
+import { PageMarkerDirective } from './page-marker.directive';
+import { PagePreloadService } from './page-preload.service';
+import { PageComponent } from './page/page.component';
+import { PasswordRequiredComponent } from './password-required/password-required.component';
+import { PasswordService } from './password.service';
+import {
+  HighlightSearchPipe,
+  SanitizeHtmlPipe,
+  SanitizeResourceHtmlPipe,
+  SanitizeStylePipe,
+} from './pipes';
+import { RenderPrintDirective } from './render-print.directive';
+import { RenderPrintService } from './render-print.service';
+import { ResizingComponent } from './resizing/resizing.component';
+import { RotationDirective } from './rotation.directive';
+import { ScrollableDirective } from './scrollable.directive';
+import { SearchService } from './search.service';
+import { SearchComponent } from './search/search.component';
+import { SearchableDirective } from './searchable.directive';
+import { SelectComponent } from './select/select.component';
+import { SidePanelComponent } from './side-panel/side-panel.component';
+import { SuccessModalComponent } from './success-modal/success-modal.component';
+import { TabActivatorService } from './tab-activator.service';
+import { TabComponent } from './tab/tab.component';
+import { TabbedToolbarsComponent } from './tabbed-toolbars/tabbed-toolbars.component';
+import { TabsComponent } from './tabs/tabs.component';
+import { TextMenuComponent } from './text-menu/text-menu.component';
+import { ThumbnailsComponent } from './thumbnails/thumbnails.component';
+import { TooltipDirective } from './tooltip.directive';
+import { TooltipComponent } from './tooltip/tooltip.component';
+import { TopTabActivatorService } from './top-tab-activator.service';
+import { TopTabComponent } from './top-tab/top-tab.component';
+import { TopToolbarComponent } from './top-toolbar/top-toolbar.component';
+import { UploadFileZoneComponent } from './upload-file-zone/upload-file-zone.component';
+import { UploadFilesService } from './upload-files.service';
+import { ViewportService } from './viewport.service';
+import { WindowService } from './window.service';
+import { ZoomDirective } from './zoom.directive';
+import { ZoomService } from './zoom.service';
 
-const providers = [ConfigService,
+const providers = [
+  ConfigService,
   Api,
   ModalService,
   FileService,
@@ -107,14 +128,15 @@ const providers = [ConfigService,
   TabActivatorService,
   AddDynamicComponentService,
   HostingDynamicComponentService,
-  TopTabActivatorService];
+  TopTabActivatorService,
+];
 
 @NgModule({
   imports: [
     CommonModule,
     FontAwesomeModule,
     NgClickOutsideDirective,
-    TranslateModule
+    TranslateModule,
   ],
   declarations: [
     TopToolbarComponent,
@@ -164,7 +186,7 @@ const providers = [ConfigService,
     TextMenuComponent,
     ContextMenuComponent,
     PageMarkerDirective,
-    ThumbnailsComponent
+    ThumbnailsComponent,
   ],
   exports: [
     TopToolbarComponent,
@@ -213,12 +235,28 @@ const providers = [ConfigService,
     TextMenuComponent,
     ContextMenuComponent,
     PageMarkerDirective,
-    ThumbnailsComponent
+    ThumbnailsComponent,
   ],
-  providers: providers
+  providers: providers,
 })
 export class CommonComponentsModule {
-  constructor() {
-    library.add(fas, far);
+  constructor(library: FaIconLibrary) {
+    // library.addIconPacks(fas, far); Not a good idea to add the whole libraries!
+    library.addIcons(
+      faCloudDownloadAlt,
+      faCheck,
+      faLevelUpAlt,
+      faCircleNotch,
+      faExclamationTriangle,
+      faCircleNotch,
+      faInfoCircle,
+      faFile,
+      faEye,
+      faChevronLeft,
+      faChevronRight,
+      faTimes,
+      faFolderOpen,
+      faKey
+    );
   }
 }
